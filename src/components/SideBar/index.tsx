9c5521b1d1ea89aa9efb657ae8205af9ab8e66c2
William Iadarola <wiadarola0@gmail.com>
2023-09-05 14:53:30 -0700

    Added better formatting for new lists and fixed render submit bug

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -24,27 +24,21 @@ export default function SideBar({ addNewList, onDeleteClick, setActiveList, list
         padding: '10px',
     };
 
-    function onAddNewClick() {
-        addNewList(newListName);
-        setNewListName('');
-    };
-
-    function openListPanel() { }
-
     return (
         <div style={sideBarStyle} id='sideBar'>
             <div style={sideBarHeader}>
-                <h1>Poop</h1>
-                <img src="https://img.icons8.com/30/16B8F3/plus.png" alt="Website" onClick={openListPanel} />
+                <h1>Lists</h1>
             </div>
+            <form id="newListForm" onSubmit={(e) => { e.preventDefault(); setNewListName(''); addNewList(newListName); }}>
+                <input type="text" value={newListName} id="newListInput" onChange={(e) => setNewListName(e.target.value)} placeholder="New list..." />
+                <img src="https://img.icons8.com/30/16B8F3/plus.png" alt="Website" onClick={(e) => { e.preventDefault(); setNewListName(''); addNewList(newListName) }} />
+            </form>
 
-            <input type="text" value={newListName} id="newListInput" onChange={(e) => setNewListName(e.target.value)} placeholder="New list..." />
-            <button onClick={() => onAddNewClick()}>Add List</button>
             <ul>
                 {lists.map((list: any, index: number) => {
                     return <li key={index}>
                         <span onClick={() => setActiveList(list.id)}>{list.name}</span>
-                        <button onClick={() => onDeleteClick(list.id)}>Delete</button>
+                        <button onClick={() => onDeleteClick(list.id)}>&#x2715;</button>
                     </li>
                 })}
             </ul>
